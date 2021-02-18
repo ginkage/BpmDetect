e59a92c838249c1b635413660fa66f5bc7652b2a
Ivan Podogov <ginkage@yandex.ru>
2021-02-18 23:19:14 +0000

    Nit: better curve straightening

--- app/src/main/cpp/wavelet_bpm_detector.cpp
+++ app/src/main/cpp/wavelet_bpm_detector.cpp
@@ -60,9 +60,8 @@ int WaveletBPMDetector::detectPeak(std::vector<float>& data)
     float max = FLT_MIN, maxP = FLT_MIN;
 
     // Straighten the curve
-    float start = data[minIndex] / (maxIndex - minIndex);
     for (int i = minIndex; i < maxIndex; ++i) {
-        data[i] = data[i] - (maxIndex - i) * start;
+        data[i] /= float(windowSize - i);
     }
 
     for (int i = minIndex; i < maxIndex; ++i) {
